cd58794bad27e14a448a4563e6e22530bd3bf28f
Daniele  Ferrillo <danyferrillo@gmail.com>
2021-02-17 12:35:34 +0100

    update parser

--- src/main/scala/com/example/parser/JsonToParse.scala
+++ src/main/scala/com/example/parser/JsonToParse.scala
@@ -1,8 +1,35 @@
 package com.example.parser
 
+
 case class JsonToParse(
-                        anno: Int,
-                        mese: Int,
-                        giorno: Int,
-                        ora : Int
+                        actor: Actor,
+                        author: String,
+                        repo: Repo,
+                        `type`: String,
+                        commit: Commit,
+                        event: Event
                       )
+
+/*
+val schema = StructType(Seq(
+      //StructField("event", StringType, false),
+      StructField("actor", StringType, true),
+      StructField("author", StringType, true),
+      StructField("repo", (new StructType), true),
+      StructField("type", StringType, true),
+      StructField("commit", StringType, true)))
+
+    val schema2 = (new StructType)
+      //StructField("event", StringType, false),
+      .add("actor", (new StructType))
+      .add("author", StringType, true)
+      .add("repo", StringType)
+      .add("type", StringType)
+      .add("commit", StringType, false)
+
+    //val schema = ScalaReflection.schemaFor[JsonToParse].dataType.asInstanceOf[StructType]
+
+    val jsonDF = sqlContext.read
+      .schema(schema)
+      .json(jsonStr)
+*/
--- src/main/scala/com/example/parser/Parser.scala
+++ src/main/scala/com/example/parser/Parser.scala
@@ -1,6 +1,8 @@
 package com.example.parser
 
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.functions._
+
 
 object Parser {
   def main(args: Array[String]): Unit = {
@@ -11,9 +13,35 @@ object Parser {
 
     val jsonStr = "C:\\Users\\Daniele\\Desktop\\LYNX\\Lynx 2° anno\\Big Data\\PROGETTO\\2018-03-01-0.json.gz"
 
-    val jsonDF = sqlContext.read.json(jsonStr)
+    val df = sqlContext.read.json(jsonStr)
+    //df.show()
 
-    jsonDF.show()
+    //Trovare i singoli «actor»
+    val actor = df.select("actor.*").distinct()
+    actor.show()
+
+    //Trovare i singoli «author», presente all’interno dei commit;
+    val payload = df.select("payload.*")
+    val commits = payload.select(explode(col("commits")))
+    val dfComm = commits.select("col.*")
+    val distinctAuthor = dfComm.select("author").distinct()
+    distinctAuthor.show()
+
+    //Trovare i singoli «repo»
+    val repo = df.select("repo.*").distinct()
+    repo.show()
+
+    //Trovare i vari tipi di evento «type»;
+    val `type` = df.select("type").distinct()
+    `type`.show()
+
+    //Contare il numero di «actor»;
+    val nActor = actor.count()
+    println(nActor)
+
+    //Contare il numero di «repo»;
+    val nRepo = repo.count()
+    println(nRepo)
 
   }
 }
